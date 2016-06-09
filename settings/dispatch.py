d8748cf4228e825fede2976680f24732cbe37186
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-09 16:45:32 -0400

    rebasing the repository

---
+++ settings/dispatch.py
@@ -0,0 +1,10 @@
+import socket
+
+#My laptop is name 'Guinsly-thinkpad-lenovo'
+if 'guinsly' in socket.gethostname():
+    from .development import *
+    print('--dev--settings--')
+else:
+    from .production import *
+    #print('prod--settings')
+#this file won't be load in git and in the
