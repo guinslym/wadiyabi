7a313cd10c210d8b8e7bb2a916d8b426f7d4c98b
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-14 00:40:19 -0400

    checking  urls

--- applications/delivrem/views.py
+++ applications/delivrem/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 #django-friendship
 from django.contrib.auth.models import User
 from friendship.models import Friend, Follow
+from django.http import HttpResponse
 
 # Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello world</h1>')
--- wadiyabi/urls.py
+++ wadiyabi/urls.py
@@ -1,15 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include, handler404, handler500
-from django.conf.urls import include, url
 from django.contrib import admin
 
 #handler404 = 'applications.websites.views.handler404'
 #handler500 = 'applications.websites.views.handler500'
+from applications.delivrem import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('applications.delivrem.urls')),
+    url(r'^$', views.hello, name='hello'),
 ]
 #Static files serves with WhiteNoise (pip install WhiteNoise)
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
