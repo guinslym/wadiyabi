bf6e53f7dbac7ab3f0bc7aac2be6d1534064a290
Guinslym <agmond@gmx.com.br>
2016-09-30 00:34:36 -0400

    adding simple admin

--- applications/delivrem/admin.py
+++ applications/delivrem/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from applications.delivrem.models import Product
+
+admin.site.register(Product)
+'''
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+'''
